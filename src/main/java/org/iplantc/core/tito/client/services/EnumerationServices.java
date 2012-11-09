92834d31b6555c6b9e1affa4b2b954b9772282c7
Dennis Roberts <dennis@iplantcollaborative.org>
2012-11-08 18:13:21 -0700

    CORE-2772: removed methods to call services that are no longer used

--- src/main/java/org/iplantc/core/tito/client/services/EnumerationServices.java
+++ src/main/java/org/iplantc/core/tito/client/services/EnumerationServices.java
@@ -6,16 +6,11 @@ import org.iplantc.de.shared.SharedAuthenticationValidatingServiceFacade;
 import org.iplantc.de.shared.services.BaseServiceCallWrapper.Type;
 import org.iplantc.de.shared.services.ServiceCallWrapper;
 
-import com.google.gwt.http.client.URL;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import org.iplantc.de.shared.SharedServiceFacade;
 
 public class EnumerationServices {
-    public void getDataFormats(AsyncCallback<String> callback) {
-        ServiceCallWrapper wrapper = new ServiceCallWrapper("org.iplantc.services.zoidberg.formats"); //$NON-NLS-1$
-        callService(callback, wrapper);
-    }
-
     public void getWidgetTypes(AsyncCallback<String> callback) {
         ServiceCallWrapper wrapper = new ServiceCallWrapper(
                 "org.iplantc.services.zoidberg.propertytypes"); //$NON-NLS-1$
@@ -27,29 +22,10 @@ public class EnumerationServices {
         callService(callback, wrapper);
     }
 
-    public void addIntegration(JSONObject tool, AsyncCallback<String> callback) {
-        ServiceCallWrapper wrapper = new ServiceCallWrapper(Type.PUT,
-                "org.iplantc.services.zoidberg.inprogress", tool.toString()); //$NON-NLS-1$
-        callService(callback, wrapper);
-    }
-
-    public void getIntegrationsAsSummary(String user, AsyncCallback<String> callback) {
-        user = URL.encode(user);
-        ServiceCallWrapper wrapper = new ServiceCallWrapper(Type.GET,
-                "org.iplantc.services.zoidberg.inprogress?user=" + user + "&summary=true"); //$NON-NLS-1$ //$NON-NLS-2$
-        callService(callback, wrapper);
-    }
-
-    public void getIntegrationAsSummary(String templateID, AsyncCallback<String> callback) {
-        ServiceCallWrapper wrapper = new ServiceCallWrapper(Type.GET,
-                "org.iplantc.services.zoidberg.inprogress?tito=" + templateID + "&summary=true"); //$NON-NLS-1$ //$NON-NLS-2$
-        callService(callback, wrapper);
-    }
-
     public void getIntegrationById(String id, AsyncCallback<String> callback) {
         ServiceCallWrapper wrapper = new ServiceCallWrapper(Type.GET,
-                "org.iplantc.services.zoidberg.inprogress?tito=" + id); //$NON-NLS-1$
-        callService(callback, wrapper);
+                "org.iplantc.services.zoidberg.inprogress/" + id); //$NON-NLS-1$
+        callSecuredService(callback, wrapper);
     }
 
     public void getUUID(AsyncCallback<String> callback) {
@@ -73,28 +49,10 @@ public class EnumerationServices {
         callService(callback, wrapper);
     }
 
-    public void saveIntegration(JSONObject tool, AsyncCallback<String> callback) {
-        ServiceCallWrapper wrapper = new ServiceCallWrapper(Type.POST,
-                "org.iplantc.services.zoidberg.inprogress", tool.toString()); //$NON-NLS-1$
-        callService(callback, wrapper);
-    }
-
-    public void deleteIntegration(JSONObject body, AsyncCallback<String> callback) {
-        ServiceCallWrapper wrapper = new ServiceCallWrapper(Type.POST,
-                "org.iplantc.services.zoidberg.inprogress", body.toString()); //$NON-NLS-1$
-        callService(callback, wrapper);
-    }
-
     public void saveAndPublish(String json, AsyncCallback<String> callback) {
         ServiceCallWrapper wrapper = new ServiceCallWrapper(Type.PUT,
-                "org.iplantc.services.zoidberg.publish?", json); //$NON-NLS-1$
-        callService(callback, wrapper);
-    }
-
-    public void getAnalysisGroups(AsyncCallback<String> callback) {
-        ServiceCallWrapper wrapper = new ServiceCallWrapper(Type.GET,
-                "org.iplantc.services.zoidberg.analgroups"); //$NON-NLS-1$
-        callService(callback, wrapper);
+                "org.iplantc.services.zoidberg.publish", json); //$NON-NLS-1$
+        callSecuredService(callback, wrapper);
     }
 
     public void getDataSources(AsyncCallback<String> callback) {
@@ -107,5 +65,9 @@ public class EnumerationServices {
 
     private void callService(AsyncCallback<String> callback, ServiceCallWrapper wrapper) {
         SharedAuthenticationValidatingServiceFacade.getInstance().getServiceData(wrapper, callback);
+    }
+
+    private void callSecuredService(AsyncCallback<String> callback, ServiceCallWrapper wrapper) {
+        SharedServiceFacade.getInstance().getServiceData(wrapper, callback);
     }
 }
